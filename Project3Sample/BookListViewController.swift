9820dea36353de301ee3c04c7c7a34609d0eee91
Matthew Pritchett <mpritchett50@gmail.com>
2019-12-03 08:06:29 -0600

    a

--- Project3Sample/BookListViewController.swift
+++ Project3Sample/BookListViewController.swift
@@ -9,8 +9,49 @@
 import UIKit
 
 class BookListViewController: UIViewController {
+    @IBOutlet weak var bookTableView: UITableView!
+    let service = BookService()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        service.fetchBooks {
+            <#code#>
+        }
+        bookTableView.dataSource = self
+        bookTableView.delegate = self
+    }
     
     
     
     
+}
+
+extension BookListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return service.books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookTableViewCell", for: indexPath) as! BookTableViewCell
+        
+        return cell
+    }
+}
+
+extension BookListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedObject = service.books[indexPath.item]
+        let detail = storyboard?.instantiateViewController(withIdentifier: "BookDetailViewController") as! BookDetailViewController
+        // detail.titleLabel.text = selectedObject.name
+        //  service.getImage(for: selectedObject.imageURL) { (url, image) in
+        //   DispatchQueue.main.async {
+        //  detail.largeImage = image
+        //  }
+        // }
+        //detail.initialTitle = selectedObject.name
+        //detail.initialURL = selectedObject.imageURL
+        //detail.service = service
+        
+        navigationController?.pushViewController(detail, animated: true)
+    }
 }
---
+++ Project3Sample/BookTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BookTableViewCell.swift
+//  Project3Sample
+//
+//  Created by Matt Pritchett on 12/2/19.
+//  Copyright © 2019 Russell Mirabelli. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+    
+}
